a63b1b6bbb63c631846e0a01ccffcd5aa630fbc5
Unknown <filip.prvulj@gmail.com>
2020-04-08 21:26:43 +0200

    Add: Initial Project

---
+++ BrainfinityWebApp/Controllers/TakmicenjeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using BrainfinityWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BrainfinityWebApp.Controllers
+{
+    public class TakmicenjeController : Controller
+    {
+        public IActionResult Index()
+        {
+            IEnumerable<TakmicenjeViewModel> takmicenja = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:5001/api/");
+
+                var response = client.GetAsync("takmicenje");
+                response.Wait();
+
+                var result = response.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var read = result.Content.ReadAsAsync<IList<TakmicenjeViewModel>>();
+                    read.Wait();
+
+                    takmicenja = read.Result;
+                }
+                else
+                {
+                    takmicenja = Enumerable.Empty<TakmicenjeViewModel>();
+
+                    ModelState.AddModelError(string.Empty, "Greska");
+                }
+            }
+
+            return View(takmicenja);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(TakmicenjeViewModel takmicenje)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:5001/api/");
+
+                var postTask = client.PostAsJsonAsync<TakmicenjeViewModel>("takmicenje", takmicenje);
+                postTask.Wait();
+
+                var result = postTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+
+            ModelState.AddModelError(string.Empty, "Greska");
+
+            return View(takmicenje);
+        }
+    }
+}
---
+++ BrainfinityWebApp/Models/TakmicenjeViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrainfinityWebApp.Models
+{
+    public class TakmicenjeViewModel
+    {
+        public int Id { get; set; }
+        public string Naziv { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd MMM yyyy}")]
+        public DateTime datumOd { get; set; }
+
+        public DateTime DatumDo { get; set; }
+    }
+}
