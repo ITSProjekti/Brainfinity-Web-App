802ef08ba0ab3eb3eb333819eba53ccbfc8752bd
Unknown <filip.prvulj@gmail.com>
2020-04-12 12:52:07 +0200

    Add: Sidebar

--- BrainfinityWebApp/wwwroot/js/site.js
+++ BrainfinityWebApp/wwwroot/js/site.js
@@ -2,3 +2,36 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$("#sidebarExpanded").click(function (e) {
+    e.preventDefault();
+
+    $(".wrapper").toggleClass("expanded");
+});
+
+$(document).ready(function () {
+    defaultScreen();
+
+    $(window).resize(function () {
+        defaultScreen();
+    }).resize();
+
+    $("#meni").on("click", ".item", function (e) {
+        var itemActive = $(this);
+        var a = itemActive.children("a");
+
+        $(".item").removeClass("active");
+        $(itemActive).addClass("active");
+
+        a.children("img").attr("src", "../images/" + a.attr("id") + "-light.png");
+
+        $("#meni > *").not(".item.active").children("a").each(function (i) {
+            $(this).children("img").attr("src", "../images/" + $(this).attr("id") + "-dark.png");
+        });
+    });
+});
+
+function defaultScreen() {
+    if ($(window).width() <= 450) {
+        $(".wrapper").addClass("expanded");
+    }
+}
