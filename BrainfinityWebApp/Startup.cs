c76192eecc8f2829e86712ab656c998d3d132c8e
F <filip.prvulj@gmail.com>
2021-01-11 04:01:45 +0100

    Add: Autorizacija i autentifikacija

--- BrainfinityWebApp/Startup.cs
+++ BrainfinityWebApp/Startup.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -25,7 +28,19 @@ namespace BrainfinityWebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddHttpClient("takmicenje", c => c.BaseAddress = new Uri("https://localhost:5001/api/"));
+            services.AddHttpContextAccessor();
+            services.AddHttpClient("takmicenje", c =>
+               {
+                   var serviceProvider = services.BuildServiceProvider();
+                   var httpContextAccessor = serviceProvider.GetService<IHttpContextAccessor>();
+
+                   c.BaseAddress = new Uri("https://localhost:44359/api/");
+
+                   if (httpContextAccessor.HttpContext.User.Identity.IsAuthenticated)
+                   {
+                       c.DefaultRequestHeaders.Add("Authorization", "Bearer " + httpContextAccessor.HttpContext.User.Claims.SingleOrDefault(t => t.Type == "token").Value);
+                   }
+               });
 
             services.AddBreadcrumbs(GetType().Assembly, options =>
             {
@@ -45,6 +60,30 @@ namespace BrainfinityWebApp
                     options.RequestCultureProviders.Clear();
                 }
             );
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(1);
+            });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/Account/Login";
+            });
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            }).AddCookie(
+                options =>
+                {
+                    options.Cookie.Name = "authCookie";
+                    options.Cookie.HttpOnly = true;
+                    options.ExpireTimeSpan = TimeSpan.FromMinutes(2d);
+                    options.AccessDeniedPath = "/Home/Index";
+                }
+                );
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -64,6 +103,9 @@ namespace BrainfinityWebApp
             app.UseRequestLocalization();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseSession();
+            app.UseAuthentication();
             app.UseCookiePolicy();
 
             app.UseMvc(routes =>
