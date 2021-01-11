84d602d4f96269d2e408caba4427e24ed6764bd7
F <filip.prvulj@gmail.com>
2021-01-11 03:55:35 +0100

    Add: TakmicenjeTabelaViewModel

---
+++ BrainfinityWebApp/ViewModels/TakmicenjeTabelaViewModel.cs
@@ -0,0 +1,14 @@
+﻿using BrainfinityWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrainfinityWebApp.ViewModels
+{
+    public class TakmicenjeTabelaViewModel
+    {
+        public IEnumerable<GrupaZadataka> GrupeZadataka { get; set; }
+        public int OdabranaGurpaZadatakaId { get; set; }
+    }
+}
